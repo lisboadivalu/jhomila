54cc7565c2c45714bdff19968d9467714dc9051f
Lisboa <lisboadivalu@gmail.com>
2021-11-13 16:42:32 -0300

    oi

--- function/load.php
+++ function/load.php
@@ -4,7 +4,7 @@ function load()
 {
     $page = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING);
 
-    $page = (!$page) ? 'pages/home.php' : "pages/{$page}.php";
+    $page = (!$page) ? 'public/pages/home.php' : "public/pages/{$page}.php";
 
     if(!file_exists($page)) {
         throw new \Exception('Acesso Negado');
